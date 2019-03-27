6fc5fcba3bb295ea06622481da5b886e7a0a6cca
Lanisya Febriyani <33479263+Lanisya@users.noreply.github.com>
2019-03-27 09:05:56 +0700

    Update MainActivity.java
    -mengubah tulisan pada canvas

--- app/src/main/java/com/example/asus_rog/belajarinputgambar/MainActivity.java
+++ app/src/main/java/com/example/asus_rog/belajarinputgambar/MainActivity.java
@@ -58,7 +58,7 @@ public class MainActivity extends Activity {
         canvas.drawLine(0,0,250,250,paint);
 
         // membuat Tulisan
-        canvas.drawText("MAYVAN GANDHY", 400, 400, paint);
+        canvas.drawText("HAI EPAN!!!", 400, 400, paint);
 
         // membuat lingkaran
         canvas.drawCircle(600,10,100,paint);
